b62b53f8e65c3e95a795bc00b19b9e207ad6a4b7
lnv0507 <lamdilinh.nguyen@gmail.com>
2022-04-25 14:05:35 +0700

    update board

--- backend/src/main/java/com/avengers/todo/controllers/BoardController.java
+++ backend/src/main/java/com/avengers/todo/controllers/BoardController.java
@@ -2,11 +2,14 @@ package com.avengers.todo.controllers;
 
 import com.avengers.todo.common.ErrorResponse;
 import com.avengers.todo.payloads.CreateBoardRequest;
+import com.avengers.todo.payloads.ResponseObject;
 import com.avengers.todo.services.BoardService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 
 
 @RestController
@@ -18,16 +21,23 @@ public class BoardController {
     private final BoardService boardService;
 
     @PostMapping
-    public ResponseEntity<?> createNewBoard(@RequestBody CreateBoardRequest request) {
+    public ResponseEntity<?> create(@RequestBody CreateBoardRequest request) {
+//        try {
+//            return ResponseEntity.ok(boardService.create(request));
+//        } catch (Exception ex) {
+//            log.error("API /api/v1/boards: ", ex);
+//            return ResponseEntity.badRequest().body(ErrorResponse.builder()
+//                    .message(ex.getMessage())
+//                    .build());
+//        }
         try {
-            return ResponseEntity.ok(boardService.create(request));
+            return ResponseEntity.status(HttpStatus.OK).body(new ResponseObject("OK", "Query Create Board Successfully", boardService.create(request)));
         } catch (Exception ex) {
             log.error("API /api/v1/boards: ", ex);
-            return ResponseEntity.badRequest().body(ErrorResponse.builder()
-                    .message(ex.getMessage())
-                    .build());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ResponseObject("failed", "Cannot Create", ""));
         }
     }
+
     @GetMapping
     public ResponseEntity<?> getAll() {
         try {
@@ -37,5 +47,4 @@ public class BoardController {
             return ResponseEntity.badRequest().body(ErrorResponse.builder().message(ex.getMessage()).build());
         }
     }
-
 }
---
+++ backend/src/main/java/com/avengers/todo/payloads/ResponseObject.java
@@ -0,0 +1,15 @@
+package com.avengers.todo.payloads;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ResponseObject {
+    private String status;
+    private String message;
+    private Object data;
+
+}
